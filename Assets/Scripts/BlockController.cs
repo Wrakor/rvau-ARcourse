d686fad471867dd3a980621cfcf9be6af0ec7367
Wrakor <nunocruz59@hotmail.com>
2015-10-08 17:27:03 +0100

    initial commit

---
+++ Assets/Scripts/BlockController.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BlockController : MonoBehaviour {
+
+	void Start(){
+		InitializeColor ();
+	}
+
+	public void InitializeColor(){
+		GetComponent<Renderer>().material.color = new Color (Random.Range (0.0f, 1.0f), Random.Range (0.0f, 1.0f), Random.Range (0.0f, 1.0f));
+	}
+	 
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	private Rigidbody rb;
+	public float speed;
+	private Vector3 initialposition;
+
+	void Start(){
+		initialposition = transform.position;
+		rb = GetComponent<Rigidbody> ();
+		rb.velocity = new Vector3 (0, 0, speed);
+	}
+
+
+
+	void OnCollisionExit(Collision collisionInfo) {
+		print ("No longer in contact with " + collisionInfo.transform.name);
+		if (collisionInfo.collider.gameObject.CompareTag ("Block")) {
+			collisionInfo.collider.gameObject.SetActive (false);
+			
+		}
+	}
+} 
