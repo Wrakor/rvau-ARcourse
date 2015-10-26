ffaa96f1376b44dff4dc53e1fe69fc68b70e218b
Wrakor <nunocruz59@hotmail.com>
2015-10-26 16:12:24 +0000

    inicializador de blocos passado para o pai; bola adicional ao partir bloco (começo da implementaçao)

---
+++ Assets/MiniballController.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MiniballController : MonoBehaviour {
+
+	void OnCollisionEnter(Collision collisionInfo) {
+
+		if (collisionInfo.gameObject.CompareTag ("EndWall")) {
+			Destroy(this.gameObject);
+		}
+	}
+}
--- Assets/Scripts/BlockController.cs
+++ Assets/Scripts/BlockController.cs
@@ -3,12 +3,25 @@ using System.Collections;
 
 public class BlockController : MonoBehaviour {
 
+	public GameObject miniball;
+
 	void Start(){
 		InitializeColor ();
 	}
 
 	public void InitializeColor(){
-		GetComponent<Renderer>().material.color = new Color (Random.Range (0.0f, 1.0f), Random.Range (0.0f, 1.0f), Random.Range (0.0f, 1.0f));
+		for (int i = 0; i < this.transform.childCount; i++)
+			GetComponentsInChildren<Renderer>()[i].material.color = new Color (Random.Range (0.0f, 1.0f), Random.Range (0.0f, 1.0f), Random.Range (0.0f, 1.0f));
+	}
+
+	void OnCollisionEnter(Collision collisionInfo) {
+
+		if (collisionInfo.gameObject.CompareTag ("Ball")) {
+			//print ("aa" + miniball.transform.position.x);
+			//
+			GameObject miniball = (GameObject) Instantiate (GameObject.FindGameObjectWithTag("Miniball"), collisionInfo.transform.position, collisionInfo.transform.rotation);
+			miniball.GetComponent<Rigidbody>().velocity = -collisionInfo.gameObject.GetComponent<Rigidbody>().velocity;
+		}
 	}
 	 
 }
